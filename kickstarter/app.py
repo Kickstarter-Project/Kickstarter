80d62d675fdc644cce8aaf1ddedf5e23e8ea4e9b
dylanbud <dylanjbudnick@gmail.com>
2021-12-20 21:28:04 -0800

    pass user input to model
    the model wouldn't load was getting an error " File "sklearn\tree\_tree.pyx", line 595, in sklearn.tree._tree.Tree.__cinit__
    ValueError: Buffer dtype mismatch, expected 'SIZE_t' but got 'long'"
    
    retrained model in 32bit python, was able to compile and load but it seems the model is wonky.
    
    Am receiving error: " UserWarning: X does not have valid feature names, but RandomForestClassifier was fitted with feature names
      warnings.warn("  in shell when flask app is running

--- kickstarter/app.py
+++ kickstarter/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request
 import pickle
 import numpy as np
+from numpy.lib.function_base import quantile
+from .model import query_model
 
 
 def create_app():
@@ -24,7 +26,8 @@ def create_app():
             currency = request.form.get('currency')
             category = request.form.get('category')
             num_backers = request.form.get('num_backers')
-        return render_template('data.html', form_data=form_data, num_backers=num_backers, campaign_name=campaign_name, campaign_length=campaign_length, percentage_pledged=percentage_pledged, currency=currency, category=category)
+            prediction = query_model(currency, category, campaign_length, percentage_pledged, num_backers)
+        return render_template('data.html', form_data=form_data, num_backers=num_backers, campaign_name=campaign_name, campaign_length=campaign_length, percentage_pledged=percentage_pledged, currency=currency, category=category, prediction=prediction)
 
     return app
 
--- kickstarter/model.py
+++ kickstarter/model.py
@@ -1,24 +1,50 @@
+# WORKING DOCUMENT #
+''' This is me trying to understand the input for the model. 
+
+input_currency will not take a string value, nor will input_category
+
+need to reconnect with roger to discuss how to format user inputted data to fit with the model '''
 
 import pickle
 import numpy as np
 
-
 model = pickle.load(open('model.pkl', 'rb'))
 
-campaign_length = 10
-percentage_pledged = 20
-backers = 20
-input_currency = 1
-input_category = 0
+list_currencies = ['AUD','CAD', 'CHF', 'DKK', 'EUR','GBP', 'HKD', 
+                 'JPY', 'MXN','NOK','NZD', 'SEK', 'SGD','USD']
+list_categories = ['Art', 'Comics', 'Crafts','Dance', 'Design','Fashion','Film & Video',
+                   'Food', 'Games','Journalism','Music', 'Photography',
+                   'Publishing', 'Technology','Theater']
 
+def currency_category_select(user_input,list_input):
+    for x in range(len(list_input)):
+        if list_input[x] == user_input:
+            list_input[x] = 1
+        else:
+            list_input[x] = 0
+    return list_input 
 
+# currency = 'HKD'
+# category = 'Fashion'
+# percentage_pledged = 95
+# input_backers = 2000
+# input_currency=currency_category_select(currency,list_currencies)
+# input_category=currency_category_select(category,list_categories)
+# campaign_length = 200
+# num_backers = 200
 
-model_input = np.array([campaign_length,percentage_pledged,backers])
-model_input = np.append(model_input,input_currency)
-model_input = np.append(model_input,input_category).reshape(1, -1)
+def query_model(currency, category, campaign_length, percentage_pledged, num_backers):
+    input_currency=currency_category_select(currency,list_currencies)
+    input_category=currency_category_select(category,list_categories)
+    model_input = np.array([campaign_length,percentage_pledged,num_backers])
+    model_input = np.append(model_input,input_currency)
+    model_input = np.append(model_input,input_category).reshape(1, -1)
 
-if model.predict(model_input)[0] == 1:
-    print('Your kickstarter is a success!')
-else: 
-    print('Your kickstarter failed, try again.')
+    if model.predict(model_input)[0] == 1:
+        prediction = 'Your kickstarter will be a success!'
+    else: 
+        prediction = "Your kickstarter will likely fail, try again!"
+
+    return(prediction)
+
 
