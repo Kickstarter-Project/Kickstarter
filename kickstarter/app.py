b55bfb0ee2cccc3a0a3eed3f15d3f302fa8bb013
dylanbud <dylanjbudnick@gmail.com>
2021-12-21 11:57:45 -0800

    fixed model
    fixed model import using joblib

--- kickstarter/app.py
+++ kickstarter/app.py
@@ -2,7 +2,8 @@ from flask import Flask, render_template, request
 import pickle
 import numpy as np
 from numpy.lib.function_base import quantile
-from .model import query_model
+from .model import model
+import pandas as pd
 
 
 def create_app():
@@ -28,8 +29,11 @@ def create_app():
             category = request.form.get('category')
             goal = request.form.get('goal')
             num_backers = request.form.get('num_backers')
-            prediction = query_model(currency, category, campaign_length, percentage_pledged, num_backers)
             month = request.form.get('month')
+            input_data = pd.DataFrame({'category':[category],'currency':[currency],
+           'goal':[goal],'launched':[month],'backers':[num_backers],
+           'campaign_length':[campaign_length],'name_char_length':[campaign_name_length]})
+            prediction = model(input_data)
         return render_template('data.html', form_data=form_data, goal=goal, month=month, campaign_name_length=campaign_name_length, num_backers=num_backers, campaign_name=campaign_name, campaign_length=campaign_length, percentage_pledged=percentage_pledged, currency=currency, category=category, prediction=prediction)
 
     return app
--- kickstarter/model.py
+++ kickstarter/model.py
@@ -6,6 +6,7 @@ input_currency will not take a string value, nor will input_category
 need to reconnect with roger to discuss how to format user inputted data to fit with the model '''
 import pandas as pd
 import pickle 
+import joblib
 
 category = 'Fashion'
 currency = 'USD'
@@ -21,11 +22,11 @@ input_data = pd.DataFrame({'category':[category],'currency':[currency],
            'campaign_length':[campaign_length],'name_char_length':[campaign_name_length]})
 
 def model(input_data):
-    rf = pickle.load(open('model.pkl', 'rb'))
+    rf = joblib.load("./random_forest_compressed.joblib")
 
     pred = rf.predict(input_data)
 
 
-    return print(pred)
+    return (pred)
 
 model(input_data)
